26f5ef111a6bee84fd06ee8c88d55732dd88b015
Yamamoto Tomomi <yamamoto@loot.co.jp>
2026-08-27 08:43:42 +0900

    Add LP samples for ケーズファイン株式会社

---
+++ ks-fine/nav.js
@@ -0,0 +1,11 @@
+// ハンバーガー開閉（全ページ共通）
+document.addEventListener('click', function (e) {
+  var btn = e.target.closest('.nav-toggle');
+  if (btn) {
+    document.querySelector('.site-header').classList.toggle('open');
+    return;
+  }
+  if (e.target.closest('.nav-menu a')) {
+    document.querySelector('.site-header').classList.remove('open');
+  }
+});
